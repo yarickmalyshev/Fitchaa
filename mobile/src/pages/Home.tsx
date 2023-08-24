a0d849a9a5f0847b497c42ad030fab304cbc300e
Sobar <sdruza4@gmail.com>
2023-08-24 20:24:44 +0300

    Initialized mobile ionic template

---
+++ mobile/src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Sir</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;

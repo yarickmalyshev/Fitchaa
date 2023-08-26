2c1abf7606497125b9d0d71c5475b9377230b62a
Sobar <sdruza4@gmail.com>
2023-08-26 13:47:53 +0300

    Finished

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,54 @@
+import { Page, Eventcalendar, getJson, Toast } from '@mobiscroll/react';
+import React from 'react';
+import './Calendar.css';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
+
+
+const Calendar = () => {
+  const [myEvents, setEvents] = React.useState([]);
+    const [isToastOpen, setToastOpen] = React.useState(false);
+    const [toastText, setToastText] = React.useState();
+
+    React.useEffect(() => {
+        getJson('https://trial.mobiscroll.com/events/?vers=5', (events) => {
+            setEvents(events);
+        }, 'jsonp');
+    }, []);
+    
+    const closeToast = React.useCallback(() => {
+        setToastOpen(false);
+    }, []); 
+    
+    const onEventClick = React.useCallback((event) => {
+        setToastText(event.event.title);
+        setToastOpen(true);
+    }, []);
+    
+    const view = React.useMemo(() => {
+        return {
+            calendar: { labels: true }
+        };
+    }, []);
+
+    return <Page>
+        <Eventcalendar
+            theme="ios" 
+            themeVariant="light"
+            clickToCreate={false}
+            dragToCreate={false}
+            dragToMove={false}
+            dragToResize={false}
+            eventDelete={false}
+            data={myEvents}
+            view={view}
+            onEventClick={onEventClick}
+       />
+        <Toast 
+    		message={toastText}
+    		isOpen={isToastOpen}
+            onClose={closeToast}
+    	/>
+    </Page>
+}
+
+export default Calendar;
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -2,17 +2,20 @@ import ImageInput from "../components/ImageInput"
 import InputGroup from '../components/InputGroup'
 import LineChart from "../components/LineChart"
 import PieChart from "../components/PieChart"
-
+import Footer from "../components/Footer"
 import edit from '../assets/edit_mark.svg';
 
 import './Admin.css'
+import Calendar from "../components/Calendar";
+import { Link } from "react-router-dom"
 
 const Admin = () => {
   return (
     <>
+      <Link className="back-button" to='/'>Назад</Link>
       <ImageInput cl='big' width='300px' height='351px' label='Добавить фото' />
       <InputGroup />
-      <h1 className="tour-title">TOUR +</h1>
+      <h1 className="tour-title">TOUR+</h1>
       <div className="tour-interface">
         <div className="avatar-display">
           <ImageInput cl='small' width='252px' height='320px' label='Добавить фото' topTitle='Ресторан “Горский”'/>
@@ -47,6 +50,11 @@ const Admin = () => {
         <PieChart />
         <LineChart />
       </div>
+      <h2 className="statics-title">Календать событий</h2>
+      <div className="calendar-box">
+        <Calendar />
+      </div>
+      <Footer />
     </>
   )
 }
